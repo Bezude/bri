96d7213e20ffd32a18b986a964f011b2b44d0706
Ben Roye <benjamin.reed.roye@gmail.com>
2015-01-10 04:28:34 -0800

    Todo creation.

--- todos.js
+++ todos.js
@@ -1,4 +1,53 @@
+
+Todos = new Mongo.Collection("todos");
+
+function Todo(name) {
+  this.name = name;
+}
+
+Todo.prototype = {
+  valid: function() {
+    return this.name && this.name != "";
+  },
+
+  save: function() {
+    Todos.insert({name: this.name});
+  }
+};
+
+function prefillTodos() {
+  if(Todos.find().count() !== 0) return;
+
+  var todos = ["Create a Todo", "Learn MeteorJS"];
+
+  todos.forEach(function(todo) { new Todo(todo).save(); });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(prefillTodos);
+}
+
+function setUpTodoTemplates() {
+  Template.todos.helpers({
+    todos: function () {
+      return Todos.find();
+    }
+  });
+
+  Template.createTodo.events({
+    'click button': function() {
+      var input = $("#newTodo");
+      var name = input.val();
+
+      input.val("")
+
+      new Todo(name).save();
+    }
+  });
+}
+
 if (Meteor.isClient) {
+  setUpTodoTemplates();
   // counter starts at 0
   Session.setDefault("counter", 0);
 
@@ -13,26 +62,5 @@ if (Meteor.isClient) {
       // increment the counter when button is clicked
       Session.set("counter", Session.get("counter") + 1);
     }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-    Todos = new Mongo.Collection("todos");
-
-    function Todo(name) {
-      this.name = name;
-    }
-
-    Todo.prototype = {
-      valid: function() {
-        return this.name && this.name != "";
-      },
-
-      save: function() {
-        Todos.insert({name: this.name});
-      }
-    };
   });
 }
