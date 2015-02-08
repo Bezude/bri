02aae0bc643fa98e0d1a32dd1e38df8904ae08e0
Benjamin Roye <benjamin.reed.roye@gmail.com>
2015-02-08 02:55:17 -0500

    Adding tasks.

--- todos.js
+++ todos.js
@@ -1,11 +1,29 @@
-// simple-todos.js
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+      console.log("Your mother was a hamster and your father smelled of elderberries.");
+      console.log(event);
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
   });
 }
